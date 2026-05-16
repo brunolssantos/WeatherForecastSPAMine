696a2d7e1b67ac02496c3d5cb2cc4dab5a2d6d7d
Bruno Santos <brunolssantos@hotmail.com>
2026-05-16 20:59:12 -0300

    Refactor code structure for improved readability and maintainability

--- services.js
+++ services.js
@@ -4,3 +4,14 @@ weatherApp.service('cityService', ['$http', function($http) {
     this.city = "New York, NY";
 
 }]);
+
+weatherApp.service('weatherService', ['$resource', function($resource) {
+
+    this.GetWeather = function(city, days) {
+        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?appid=XXXX", {
+            callback: "JSON_CALLBACK"
+        }, { get: { method: "JSONP" } });
+        return weatherAPI.get({ q: city, cnt: days });
+    };
+
+}]);
